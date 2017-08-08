14ad8cd1ac9648bd1aca2b18d62b277ed479697a
alserge <30702762+alserge@users.noreply.github.com>
2017-08-08 23:22:23 +0400

    A couple of functions were added

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,20 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+  public static void main(String[] args) {
+    double l = 5;
+    double a = 3;
+    double b = 2;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+  }
+
+  public static double area(double ven) {
+    return ven * ven;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 
 }
