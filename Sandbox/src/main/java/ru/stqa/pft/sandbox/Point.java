021b1ac4e7d393584af0494eeb52042c437687ef
alserge <30702762+alserge@users.noreply.github.com>
2017-08-13 21:39:54 +0400

    Создание класса Point

---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  public int x;
+  public int y;
+
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+  public double distance() {
+    return Math.sqrt(((this.x - this.x) * (this.x - this.x)) + ((this.y - this.y) * (this.y - this.y)));
+  }
+
+}
+
+
+
