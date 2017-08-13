54cddc8f1c5bc6e9e01b7e73c3e5ab17536b1fd1
alserge <30702762+alserge@users.noreply.github.com>
2017-08-13 12:55:48 +0400

    Повторение - мать учения

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,19 +3,6 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    double l = 5;
-    double a = 3;
-    double b = 2;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    System.out.println("Hello, world!");
   }
-
-  public static double area(double ven) {
-    return ven * ven;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
-
 }
